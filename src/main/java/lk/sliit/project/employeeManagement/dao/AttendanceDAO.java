6005be8a90669806da6cde0ca47dd71d2f3a878b
rivinduc <trivinduchamath333@gmail.com>
2020-04-28 20:57:36 +0530

    D

--- src/main/java/lk/sliit/project/employeeManagement/dao/AttendanceDAO.java
+++ src/main/java/lk/sliit/project/employeeManagement/dao/AttendanceDAO.java
@@ -1,7 +1,6 @@
 package lk.sliit.project.employeeManagement.dao;
 
 import lk.sliit.project.employeeManagement.entity.Attendance;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import java.util.Date;
 
@@ -17,6 +16,7 @@ public interface AttendanceDAO extends CrudRepository<Attendance, String> {
 
     //Get Today attendance
     Iterable<Attendance> findAttendanceByDateEquals(Date date);
-    @Query(value = "SELECT overtimeHours FROM attendance WHERE employeeId=?1", nativeQuery = true)
-    double getEmp(String idNo);
+
+//    @Query(value = "SELECT overtimeHours FROM attendance WHERE employeeId=?1", nativeQuery = true)
+//    double getEmp(String idNo);
 }
